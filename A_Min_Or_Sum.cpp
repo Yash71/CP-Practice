c8c68b0049f5f57fe2f3f5154e1e934121860abd
yash <yashrajesh71@gmail.com>
2022-02-21 18:52:30 +0530

    CP Practice 800

--- A_Min_Or_Sum.cpp
+++ A_Min_Or_Sum.cpp
@@ -1,38 +1,18 @@
 #include<iostream>
-#include<vector>
-using namespace std; 
+using namespace std;
 
-int main(){ 
-
+int main(){
     int t;
     cin>>t;
     while(t--){
         int n;
         cin>>n;
-        int element;
-        int j,maxSize=0;
-        vector<int> ans(100,0);
+        int x,ans=0;
         for(int i=0;i<n;i++){
-            cin>>element;
-            j=0;
-            while(element>0){
-                ans[j]=max(element%2,ans[j]);
-                element/=2;
-                j++;
-            }
-            maxSize=max(maxSize,j);
+            cin>>x;
+            ans|=x;
         }
-        int sum=0;
-        int p=1;
-        
-        // for(int i=0;i<=maxSize;i++) cout<<ans[i];
-        // cout<<"\n";
-        for(int i=1;i<=maxSize;i++){
-            p*=2;
-            sum += p * ans[i];
-        }
-        sum+=ans[0];
-        cout<<sum<<"\n";
+        cout<<ans<<"\n";
     }
     return 0;
 }
--- B_Avoid_Local_Maximums.cpp
+++ B_Avoid_Local_Maximums.cpp
@@ -1,12 +1,36 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
 int main(){
     int t;
     cin>>t;
     while(t--){
-        
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int count=0;
+        for(int i=1;i<n-1;i++){
+            if(a[i]>a[i+1] && a[i]>a[i-1]){
+                if(i+2<n){
+                a[i+1]=max(a[i],a[i+2]);
+                }
+                else{
+                    a[i+1]=a[i];
+                }
+                count++;
+            }
+        }
+        cout<<count<<"\n";
+        for(auto it: a) cout<<it<<" ";
+        cout<<"\n";
     }
     return 0;
     
 }
+
+// 2 1 3 1 3 1 3 1 3 
