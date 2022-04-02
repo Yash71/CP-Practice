0417dcd3718fbb0add032f91cfd247429c6ee79b
yash <yashrajesh71@gmail.com>
2022-04-02 16:43:21 +0530

    CP Practice 800

--- A_Jeff_and_Digits.cpp
+++ A_Jeff_and_Digits.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
     int n;
     cin >> n;
-    int x, count0 = 0;
+    int arr[n];
+    int count5 = 0, count0 = 0;
+    int x;
     for (int i = 0; i < n; i++)
     {
         cin >> x;
-        if (x == 0)
-            count0++;
+        x == 5 ? count5++ : count0++;
     }
-    if (count0 == 0)
-        cout << "-1"
-             << "\n";
-    else
+    if (count0 != 0)
     {
-        if (n % 2 != 0)
+        if (count5 < 9)
+            cout << "0"
+                 << "\n";
+        else
         {
-
-            for (int i = 0; i < n - count0 - 1; i++)
+            count5 -= count5 % 9;
+            for (int i = 0; i < count5; i++)
             {
                 cout << "5";
             }
             for (int i = 0; i < count0; i++)
                 cout << "0";
         }
-        else cout<<"0"<<"\n";
     }
+    else
+        cout << "-1"
+             << "\n";
     return 0;
 }
---
+++ B_Balanced_Array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std; 
+
+int main(){ 
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%4==0){
+            cout<<"YES"<<"\n";
+            for(int i=1;i<=n/2;i++){
+                cout<<i*2<<" ";
+            }
+            for(int i=1;i<n/2;i++){
+                cout<<i*2-1<<" ";
+            }
+            cout<<n+n/2-1<<"\n";
+        }
+        else cout<<"NO"<<"\n";
+    }
+    return 0;
+}
